1a2ce9a1978703b66170f650aec8730cca87df3c
Eduardo <edsilva7878@gmail.com>
2022-08-24 21:18:42 -0300

    correcao da foto

--- src/pages/info/index.jsx
+++ src/pages/info/index.jsx
@@ -17,7 +17,7 @@ export default function Info() {
         <a className='fonts' target="_blank" href="https://cloud.google.com">cloud.google.com</a>
         <a className='fonts' target="_blank" href="https://www.embratel.com.br/cloud/servicos-em-nuvem">embratel.com.br/cloud</a>
         <a className='fonts' target="_blank" href="https://github.com/edsilva7878">github.com/edsilva7878</a>
-        <a className='fonts' target="_blank" href="https://www.ccoe.netlify.app">ccoe.netlify.app</a>
+        <a className='fonts' target="_blank" href="https://ccoe.netlify.app/">ccoe.netlify.app</a>
         </div>
           <div className='footer'>
               <Link to='/services'><IoIosArrowBack className='arrowLeftWhite'/></Link>
